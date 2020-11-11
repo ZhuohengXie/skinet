743415397dc2491991277f7393afc13bb0724e9d
Zhuoheng Xie <zh-xie@live.com>
2020-11-11 19:57:30 +0800

    End of Section 6

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -18,6 +18,7 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
         {
+            
             return await _context.ProductBrands.ToListAsync();
         }
 
